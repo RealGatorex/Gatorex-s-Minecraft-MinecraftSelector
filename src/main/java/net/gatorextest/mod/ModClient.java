fe7a6b29df56202de3db79c1920cbbe1dfd0eee8
Xerooqq <gatorexczofficial@gmail.com>
2026-08-05 02:09:05 +0200

    Initial Commit

---
+++ src/main/java/net/gatorextest/mod/ModClient.java
@@ -0,0 +1,10 @@
+package net.gatorextest.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
